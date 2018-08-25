a09c4a81c98405acd221be63bb9e70d62da82326
SuxrobGM <suxrobGM@gmail.com>
2018-08-25 20:28:59 +0500

    added base files

---
+++ TestGitLab.API/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitLab.API;
+
+namespace TestGitLab.API
+{
+    class Program
+    {
+        private static string Token = "yHe-Kh9yJ-Uqzy22H3dZ";
+
+        static void Main(string[] args)
+        {
+            GitLabClient client = new GitLabClient(Token);
+            //Console.WriteLine(client.GetProjectDataAsync(6500892).Result.ToString());
+            //Console.WriteLine(client.GetProjectIdAsync("Economic Crisis").Result.ToString());
+            Console.WriteLine(client.GetRepositoryTreeAsync(6500892, true).Result.ToString());
+
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        }
+    }
+}
